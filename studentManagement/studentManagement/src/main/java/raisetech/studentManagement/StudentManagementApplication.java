2bc3a6b3cb5975fe355858b41cd6b8caf4b24558
devac-tech <sigoto.yamano@gmail.com>
2025-07-07 15:22:06 +0900

    Add map processing

--- studentManagement/studentManagement/src/main/java/raisetech/studentManagement/StudentManagementApplication.java
+++ studentManagement/studentManagement/src/main/java/raisetech/studentManagement/StudentManagementApplication.java
@@ -1,20 +1,37 @@
 package raisetech.studentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name;
+	private String age;
+	private Map<String, String> student = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World!";
+	@GetMapping("/studentInfo")
+	public Map<String, String> getStudentInfo() {
+		return student;
 	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(@RequestParam String name, @RequestParam String age) {
+		student.put("name", name);
+		student.put("age", age);
+	}
+
 }
