4d82fc3c2c41ad6d4047136617b5c82bef053126
devac-tech <sigoto.yamano@gmail.com>
2025-07-08 14:30:46 +0900

    Add student CRUD

--- studentManagement/studentManagement/src/main/java/raisetech/studentManagement/StudentManagementApplication.java
+++ studentManagement/studentManagement/src/main/java/raisetech/studentManagement/StudentManagementApplication.java
@@ -3,10 +3,13 @@ package raisetech.studentManagement;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagementApplication {
 
+	@Autowired
+	private StudentRepository repository;
+
 	private String name;
-	private String age;
-	private Map<String, String> student = new HashMap<>();
+	private int age;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/studentInfo")
-	public Map<String, String> getStudentInfo() {
-		return student;
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name) {
+		Student student = repository.searchByNames(name);
+		return student.getName() + " " + student.getAge() + "歳";
 	}
 
-	@PostMapping("/studentInfo")
-	public void setStudentInfo(@RequestParam String name, @RequestParam String age) {
-		student.put("name", name);
-		student.put("age", age);
+	@PostMapping("/student")
+	public void resisterStudent(String name, int age) {
+		this.name = name;
+		this.age = age;
 	}
 
+	@PatchMapping("/student")
+	public void updateStudentName(int age) {
+		repository.updateStudent(age);
+	}
+
+	@DeleteMapping("/student")
+	public void deleteStudent(String name){
+		repository.deleteStudent(name);
+	}
 }
